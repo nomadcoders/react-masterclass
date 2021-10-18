c6172551866150fc93e30d7896ebb2cc8d1687c6
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-10-18 23:42:30 +0900

    1.3 'As' and Attrs

--- src/App.js
+++ src/App.js
@@ -3,20 +3,19 @@ import styled from "styled-components";
 const Father = styled.div`
   display: flex;
 `;
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
-const Circle = styled(Box)`
-  border-radius: 50px;
+
+const Input = styled.input.attrs({ required: true })`
+  background-color: tomato;
 `;
 
 function App() {
   return (
-    <Father>
-      <Box bgColor="teal" />
-      <Circle bgColor="tomato" />
+    <Father as="header">
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
     </Father>
   );
 }
