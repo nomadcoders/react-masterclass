0d9a58c1c1894cbb820e9972b5311d801708ca47
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-10-18 18:07:43 +0900

    1.1 Our First Styled Component

---
+++ README.md
@@ -0,0 +1,14 @@
+# React Masterclass
+
+Learn and dominate the React ecosystem once and for all.
+
+### Using:
+
+- Styled Components
+- Recoil
+- React Query
+- React Router DOM
+- Apex Charts
+- React Spring
+- Typescript
+- Firebase
---
+++ src/App.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+const BoxOne = styled.div`
+  background-color: teal;
+  width: 100px;
+  height: 100px;
+`;
+const BoxTwo = styled.div`
+  background-color: tomato;
+  width: 100px;
+  height: 100px;
+`;
+const Text = styled.span`
+  color: white;
+`;
+
+function App() {
+  return (
+    <Father>
+      <BoxOne>
+        <Text>Hello</Text>
+      </BoxOne>
+      <BoxTwo />
+    </Father>
+  );
+}
+
+export default App;
