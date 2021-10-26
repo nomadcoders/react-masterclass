6aec07ece7c2806bdd4a342de259f8d1f658a082
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-10-26 21:44:00 +0900

    4.15 Inmmutability part Two

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useSetRecoilState } from "recoil";
 import { IToDo, toDoState } from "../atoms";
 
+const food = ["pizza", "mango", "kimchi", "kimbab"];
+
 function ToDo({ text, category, id }: IToDo) {
   const setToDos = useSetRecoilState(toDoState);
   const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -10,8 +12,12 @@ function ToDo({ text, category, id }: IToDo) {
     } = event;
     setToDos((oldToDos) => {
       const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
-      const newToDo = { text, id, category: name };
-      return oldToDos;
+      const newToDo = { text, id, category: name as any };
+      return [
+        ...oldToDos.slice(0, targetIndex),
+        newToDo,
+        ...oldToDos.slice(targetIndex + 1),
+      ];
     });
   };
   return (
